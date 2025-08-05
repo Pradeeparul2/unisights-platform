04331355d01a06df052b1a30681ab96e27932e93
Pradeep A <pradeep.a@vuedata.in>
2025-08-05 23:18:04 +0530

    Superset durid DB setup completed

--- docker/superset/init-superset.sh
+++ docker/superset/init-superset.sh
@@ -20,5 +20,11 @@ fi
 # Initialize Superset
 superset init
 
+
+# Add Druid database
+superset set-database-uri \
+  --database_name Druid \
+  --uri "druid://broker:8082/druid/v2/sql/"
+
 # Start Gunicorn
 gunicorn -w 4 --timeout 120 -b 0.0.0.0:8088 'superset.app:create_app()'
